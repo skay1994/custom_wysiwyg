cbf9312f501840ea18255a435cdc5be99eaba510
Jorge Carlos <skay_1994@yahoo.com.br>
2022-02-23 15:58:04 -0300

    Added index.js

---
+++ src/index.js
@@ -0,0 +1,8 @@
+process.title = 'MyWebServer';
+let args = process.argv,
+port = args[2] || 7070,
+webServer = require('./server');
+
+webServer.listen(port, function() {
+    console.log('Server started at port ' + port);
+});
